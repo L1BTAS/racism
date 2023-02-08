f7dd17e078c1d008b14354138cb5dd23051f93a9
nik-marinenko <nik.marinenko@gmail.com>
2023-02-08 15:59:33 +0300

    Вылет с карты через обочину

--- Assets/scripts/Trigger.cs
+++ Assets/scripts/Trigger.cs
@@ -16,16 +16,17 @@ public class Trigger : MonoBehaviour
     private void OnTriggerStay2D(Collider2D other)
     {
        
-            other.GetComponent<Rigidbody2D>().gravityScale = 30;
+        other.GetComponent<Rigidbody2D>().gravityScale = 30;
+        other.GetComponent<MovementCarPlayer>().mapBottom = -8.5f;
 
-        
-        
+
+
     }
     private void OnTriggerExit2D(Collider2D other)
     {
 
         other.GetComponent<Rigidbody2D>().gravityScale = 0;
-
+        other.GetComponent<MovementCarPlayer>().mapBottom = -5.5f;
 
 
     }
