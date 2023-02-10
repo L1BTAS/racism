a6e13c6ebda1e02d23b8dcf2207702fd8274ab17
Wiash01 <aleev.1337@gmail.com>
2023-02-10 22:19:59 +0300

    меню настроечееееек (говядина)
    ну там кароче графон можна выбирать, звук громче тише(ваще крута сделал что оно после выхода сохраняется), рез себе поставить крутой, а еще карочи фулл скрин бабахнуть можна себе

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public TMP_Dropdown resolutionDropdown;
+
+
+    public float volumeValue;
+    public Slider volumeSlider;
+
+    Resolution[] resolutions;
+
+    
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+
+            options.Add(option);
+
+            if (resolutions[i].width==Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+        //весь старт нужен был что бы определять разрешение твоего монитора и ставить его автоматически
+
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+
+
+
+    }
+
+    void Update()
+    {
+        PlayerPrefs.SetFloat("volume", volumeValue);
+
+
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+
+    public void SetVolume (float volume)
+    {
+        volumeValue = volume;
+
+
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+
+        QualitySettings.SetQualityLevel(qualityIndex);
+
+    }
+
+    public void FullScreen(bool IsFoolscreen)
+    {
+        Screen.fullScreen = IsFoolscreen;
+    }
+}
