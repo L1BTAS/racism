fb8d3cf4c27e23f66c9fdb1a54cfa3b587e68d59
Wiash01 <aleev.1337@gmail.com>
2023-02-13 20:46:01 +0300

    настроена скорость спавна и скорость спавна машинок
    ну оно вроде работает +- сложно и иногда даже по 2 спавница я ваще маладец :)

--- Assets/scripts/Traffic/TrafficSpawner.cs
+++ Assets/scripts/Traffic/TrafficSpawner.cs
@@ -17,19 +17,27 @@ public class TrafficSpawner : MonoBehaviour
         if(Time.time >= timeToSpawn)
         {
             SpawnTraffic();
-            timeToSpawn = Time.time + timeBetweenSpawns;
+            timeToSpawn = Time.time + timeBetweenSpawns- Time.timeSinceLevelLoad / 500;
         }
 
     }
 
     void SpawnTraffic()
     {
+        int randomIndex2 = Random.Range(0, spawnPoints.Length);
+        int randomIndex3 = Random.Range(0, spawnPoints.Length);
         int randomIndex = Random.Range(0, spawnPoints.Length);
         int randomTraffic = Random.Range(0, trafficPrefab.Length);
+        int randomTraffic2 = Random.Range(0, trafficPrefab.Length);
         for (int i = 0; i < spawnPoints.Length; i++)
         {
             if (randomIndex == i)
             {
+                if (randomIndex == randomIndex2)
+                {
+
+                    Instantiate(trafficPrefab[randomTraffic2], spawnPoints[randomIndex3].position, Quaternion.identity);
+                }
                 Instantiate(trafficPrefab[randomTraffic], spawnPoints[i].position, Quaternion.identity);
             }
         }
