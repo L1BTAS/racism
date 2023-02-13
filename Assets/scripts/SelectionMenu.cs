f41612ebc326650a9b9dd7dde1b14d127349e145
nik-marinenko <nik.marinenko@gmail.com>
2023-02-13 19:59:43 +0300

    bugfix
    -скрывание playbutton если не выбрана машинка

--- Assets/scripts/SelectionMenu.cs
+++ Assets/scripts/SelectionMenu.cs
@@ -5,9 +5,34 @@ using UnityEngine.UI;
 
 public class SelectionMenu : MonoBehaviour
 {
+    private int playersCount = 1;
+    public GameObject playButton;
+
     void Start()
     {
         Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        playersCount = 6;
+        for (int i = 0; i < 6; i++)
+        {
+            if (GameObject.FindGameObjectWithTag("Player" + i) == null)
+            {
+                playersCount -= 1;
+            }
+
+        }
+
+        if(playersCount == 0)
+        {
+            playButton.SetActive(false);
+        }
+        else
+        {
+            playButton.SetActive(true);
+        }
     }
     public void StartGame()
     {
